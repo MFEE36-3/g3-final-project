490a7cc9660f9ad1e721e39acbba081f3a0a8196
Daidai <sc931612@gmail.com>
2023-07-05 02:13:28 +0800

    add fonts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import LayoutMainPage from "@/components/layout/layout-mainpage"
-import styles from '@/styles/mainpage.module.css'
-import Image from "next/image"
-import chocoCookie from '@/public/main_page/chocoCookie.svg'
-import slogan from '@/public/main_page/slogan.svg'
-import walkbag_line from '@/public/main_page/walkbag_line.svg'
-import filled_walkbag_middle from '@/public/main_page/filled_walkbag_middle.svg'
-import filled_walkbag_last from '@/public/main_page/filled_walkbag_last.svg'
+import LayoutMainPage from "@/components/layout/layout-mainpage";
+import styles from '@/styles/mainpage.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from "next/image";
+import chocoCookie from '@/public/main_page/chocoCookie.svg';
+import slogan from '@/public/main_page/slogan.svg';
+import walkbag_line from '@/public/main_page/walkbag_line.svg';
+import filled_walkbag_middle from '@/public/main_page/filled_walkbag_middle.svg';
+import filled_walkbag_last from '@/public/main_page/filled_walkbag_last.svg';
 import shadow from '@/public/main_page/shadow.svg';
-import Btn from "@/components/common/btn"
+import Btn from "@/components/common/btn";
+import sugoeat from '@/public/main_page/sugoeat.svg';
 
 const Home = () => {
 
@@ -19,19 +21,25 @@ const Home = () => {
         <Image src={chocoCookie} className={styles.chocoCookie} alt='chocoCookie with site name' priority />
       </div>
 
-      <div className={styles.buy_for_me}>
-        <div className={styles.buyme_left}>
-          <Image src={walkbag_line} className={styles.walkbag_line}
-            alt='walkbag_line'/>
-        </div>
-        <Image src={filled_walkbag_middle} className={styles.filled_walkbag_middle} alt='filled_walkbag_middle'/>
-        <Image src={filled_walkbag_last} className={styles.filled_walkbag_last} alt='filled_walkbag_last'/>
-        <Image src={shadow} className={styles.shadow} alt='shadow'/>
-        
-        <div className="buyme_right">
-          <Btn text='馬上GOGO!'/>
+      <div className={`${styles.buy_for_me}`}>
+        <div className={`${styles.buyme_left}`}>
+          <Image src={walkbag_line} className={`${styles.walkbag_line}`}
+            alt='walkbag_line' />
+          <Image src={filled_walkbag_middle} className={styles.filled_walkbag_middle} alt='filled_walkbag_middle' />
+          <Image src={filled_walkbag_last} className={styles.filled_walkbag_last} alt='filled_walkbag_last' />
+          <Image src={shadow} className={styles.shadow} alt='shadow' />
         </div>
 
+        <div className={`${styles.buyme_right}`}>
+          <Btn text='馬上GOGO!' />
+        </div>
+
+      </div>
+      <div className={styles.contact}>
+        <Image src={sugoeat} alt='sugoeat' style={{width:'100%', height: 'auto'}}></Image>
+        <div>
+          <p className={styles.contact_us}>聯絡我們</p>
+        </div>
       </div>
     </>
   )
