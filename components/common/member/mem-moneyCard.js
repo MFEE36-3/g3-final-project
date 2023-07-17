0681ca14609d40b45cf7cd346904c94a3bd006c0
luke1128qq <ggeniussquid@gmail.com>
2023-07-17 10:36:58 +0800

    0717

--- components/common/member/mem-moneyCard.js
+++ components/common/member/mem-moneyCard.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import styles from './mem-moneyCard.module.css';
+import Link from 'next/link';
 
 export default function MemMoneyCard() {
   return (
     <div className={styles.cardBox}>
-      <div className={styles.card}>
-        <p>尊榮會員一個月</p>
-        <p>199元</p>
-      </div>
-      <div className={styles.card}>
-        <p>尊榮會員三個月</p>
-        <p>549元</p>
-      </div>
-      <div className={styles.card}>
-        <p>尊榮會員六個月</p>
-        <p>999元</p>
-      </div>
-      <div className={styles.card}>
-        <p>尊榮會員一年</p>
-        <p>1799元</p>
-      </div>
+      <Link href={''} className={styles.link}>
+        <div className={styles.card}>
+          <p>尊榮會員一個月</p>
+          <p>199元</p>
+        </div>
+      </Link>
+      <Link href={''} className={styles.link}>
+        <div className={styles.card}>
+          <p>尊榮會員三個月</p>
+          <p>549元</p>
+        </div>
+      </Link>
+      <Link href={''} className={styles.link}>
+        <div className={styles.card}>
+          <p>尊榮會員六個月</p>
+          <p>999元</p>
+        </div>
+      </Link>
+      <Link href={''} className={styles.link}>
+        <div className={styles.card}>
+          <p>尊榮會員一年</p>
+          <p>1799元</p>
+        </div>
+      </Link>
     </div>
   );
 }
