868bba261312c32c97ae3d540294e9a48d83e55e
luke1128qq <ggeniussquid@gmail.com>
2023-07-31 12:57:21 +0800

    try fix css

--- components/member/mem-moneyCard.js
+++ components/member/mem-moneyCard.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import styles from './mem-moneyCard.module.css';
 import Link from 'next/link';
+import { v4 } from 'uuid';
+import { useRouter } from 'next/router';
+import AuthContext from '@/context/AuthContext';
+import { useContext } from 'react';
 
 export default function MemMoneyCard() {
+  const { auth } = useContext(AuthContext);
+
+  const sendData = (e) => {
+    console.log(e.target.textContent);
+    const cardData = {
+      // 會員id
+      // 內容
+      // 價格
+    };
+  };
+
   return (
     <div className={styles.cardBox}>
-      <Link href={''} className={styles.link}>
-        <div className={styles.card}>
-          <p>尊榮會員一個月</p>
-          <p>199元</p>
-        </div>
-      </Link>
-      <Link href={''} className={styles.link}>
-        <div className={styles.card2}>
-          <p>尊榮會員三個月</p>
-          <p>549元</p>
-        </div>
-      </Link>
-      <Link href={''} className={styles.link}>
-        <div className={styles.card3}>
-          <p>尊榮會員六個月</p>
-          <p>999元</p>
-        </div>
-      </Link>
-      <Link href={''} className={styles.link}>
-        <div className={styles.card4}>
-          <p>尊榮會員一年</p>
-          <p>1799元</p>
-        </div>
-      </Link>
+      <button className={styles.card} onClick={sendData}>
+        尊榮會員一個月199元
+      </button>
+      <button className={styles.card2} onClick={sendData}>
+        尊榮會員三個月549元
+      </button>
+      <button className={styles.card3} onClick={sendData}>
+        尊榮會員六個月999元
+      </button>
+      <button className={styles.card4} onClick={sendData}>
+        尊榮會員12個月1799元
+      </button>
     </div>
   );
 }
