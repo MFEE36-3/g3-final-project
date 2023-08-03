070ccd00954138f0e5282bf55b44aba7b45c3cfe
michael <michaellin5027@gmail.com>
2023-08-04 00:03:06 +0800

    select-rating

--- components/reservation/main/index.js
+++ components/reservation/main/index.js
@@ -4,20 +4,20 @@ import MainContent from './maincontent';
 import { useRouter } from 'next/router';
 import ResultContent from './resultcontent';
 
-export default function Main({ keyword, setKeyword ,favorite,setFavorite }) {
+export default function Main({ keyword, setKeyword, favorite, setFavorite }) {
 
   const router = useRouter();
-  console.log(router)
+  // console.log(router)
   return (
     <>
       {/* 判斷router.query來決定顯示的內容 */}
       {Object.keys(router.query).length === 0 ?
         (<div >
-          <Top5/>
-          <MainContent favorite={favorite} setFavorite={setFavorite}/>
+          <Top5 />
+          <MainContent favorite={favorite} setFavorite={setFavorite} />
         </div>)
         : (
-          <ResultContent favorite={favorite} setFavorite={setFavorite}/>)
+          <ResultContent favorite={favorite} setFavorite={setFavorite} />)
       }
     </>
   );
