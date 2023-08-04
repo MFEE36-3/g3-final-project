ef9afa45602adea5838859768f23ef84a5fef144
mouse851130 <chou.no98@gmail.com>
2023-08-05 04:00:13 +0800

    complete forum like & add judgement

--- components/common/forum/articlelist.js
+++ components/common/forum/articlelist.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import styles from './articlelist.module.css';
 import { BsSuitHeartFill } from 'react-icons/bs';
 import { BiSolidMessageAltDetail } from 'react-icons/bi';
 import { FaRegBookmark } from 'react-icons/fa';
+import { FiHeart } from 'react-icons/fi'        // 空心愛心
+import { BiSolidHeart } from 'react-icons/bi'   // 實心愛心
 import Link from 'next/link';
 import dayjs from 'dayjs';
+import AuthContext from '@/context/AuthContext';
+import Swal from 'sweetalert2'
+import { useRouter } from 'next/router';
 
 export default function Articlelist({
   data = [],
@@ -12,12 +17,103 @@ export default function Articlelist({
   imgPreview = '',
   articles = [],
 }) {
+  console.log(articles)
+  const router = useRouter()
   const [messageCounts, setMessageCounts] = useState({});
+  const { auth, setAuth, logout } = useContext(AuthContext)
+  const [clickHeart, setClickHeart] = useState(false)
+  const [sendLike, setSendLike] = useState({
+    member_id: auth.sid,
+    clickHeart: false,
+    article_sid: ''
+  })
+
+  const getMemberId = () => {
+    setSendLike({ ...sendLike, member_id: auth.sid, })
+  }
+
+  // 網頁一進入就抓取會員按過哪些文章讚
+  useEffect(() => {
+
+  }, [])
+
+  // 設置一個接住會員按讚的狀態
+  const [memberLike, setMemberLike] = useState([])
+  useEffect(() => {
+    if (auth.account) {
+      getMemberId()
+
+      fetch(process.env.API_SERVER + '/forum/get-member-like', {
+        method: 'POST',
+        body: JSON.stringify(auth),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(r => r.json())
+        .then(data => {
+          console.log(data)
+          setMemberLike(data)
+
+        })
+    }
+  }, [auth])
+  console.log(memberLike)
+  useEffect(() => {
+    // 在這裡處理後續程式碼，這裡的sendLike.article_sid將獲得最新值
+    console.log("最新的 article_sid:", sendLike.article_sid);
+
+    // 在這裡執行後續程式碼
+    if (sendLike.article_sid) {
+      fetch(process.env.API_SERVER + '/forum/handle-like-list', {
+        method: 'POST',
+        body: JSON.stringify(sendLike),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(r => r.json())
+        .then(data => {
+          console.log(data);
+        });
+    }
+  }, [sendLike.article_sid, sendLike.clickHeart]);
+
+  const clickHeartEvent = async (e, article_sid) => {
+    if (auth.account) {
+      console.log('有帳號')
+      e.preventDefault();
+      setClickHeart(!clickHeart);
+      setSendLike(prevSendLike => ({
+        ...prevSendLike,
+        article_sid: article_sid,
+        clickHeart: !clickHeart
+      }));
+    } else {
+      console.log('沒有帳號')
+      Swal.fire({
+        title: '您尚未登入',
+        text: "要登入以編輯網站嗎?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '帶我去登入'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          router.push('/login')
+        } else {
+
+        }
+      })
+    }
+  };
 
   return (
     <>
-      {articles.map((c) => (
-        <div key={c.forum_sid} className={styles.right}>
+      {articles.map((c, i) => {
+        const hasLiked = memberLike.some(item => item.forum_sid === c.forum_sid);
+        return <div key={c.forum_sid} className={styles.right}>
           <div className={styles.container}>
             <div className={styles.flex}>
               <div className={styles.avatar}>
@@ -45,7 +141,30 @@ export default function Articlelist({
               </div>
             </Link>
             <div className={styles.flex2}>
-              <BsSuitHeartFill className={styles.icon} />
+            {hasLiked ? (
+                <BiSolidHeart
+                  className={styles.icon}
+                  onClick={(e) => {
+                    clickHeartEvent(e, c.forum_sid);
+                  }}
+                />
+              ) : (
+                <FiHeart
+                  className={styles.icon}
+                  onClick={(e) => {
+                    clickHeartEvent(e, c.forum_sid);
+                  }}
+                />
+              )}
+
+              {/* {i + 1 == memberLike[i].forum_sid ? <FiHeart className={styles.icon} onClick={(e) => { clickHeartEvent(e, c.forum_sid) }} />
+                :
+                <BiSolidHeart className={styles.icon} onClick={(e) => { clickHeartEvent(e, c.forum_sid) }} />} */}
+              {/* {sendLike.clickHeart == false ? 
+              <FiHeart className={styles.icon} onClick={(e) => { clickHeartEvent(e, c.forum_sid) }} /> 
+              : 
+              <BiSolidHeart className={styles.icon} onClick={(e) => { clickHeartEvent(e, c.forum_sid) }} />} */}
+
               <div className={styles.like}>{c.like_count}</div>
               <BiSolidMessageAltDetail className={styles.message} />
               <div className={styles.like}>{c.comment_count}</div>
@@ -54,7 +173,7 @@ export default function Articlelist({
             </div>
           </div>
         </div>
-      ))}
+      })}
     </>
   );
 }
--- components/common/forum/hotnew.js
+++ components/common/forum/hotnew.js
@@ -1,8 +1,47 @@
-import React from 'react';
+// import React from 'react';
 import styles from './hotnew.module.css';
 import { BsPencilSquare } from 'react-icons/bs';
 import Link from 'next/link';
+import React, { useEffect, useState, useContext } from 'react';
+import AuthContext from '@/context/AuthContext';
+import Swal from 'sweetalert2'
+import { useRouter } from 'next/router';
+
 export default function Hotnew() {
+  const { auth, setAuth, logout } = useContext(AuthContext)
+  const router = useRouter()
+
+  const [catchSid, setCatchSid] = useState('')
+  useEffect(()=>{
+    if(auth.account){
+      setCatchSid(auth.account)
+    }
+  },[auth])
+
+  const checkLogin = (e)=>{
+    if(auth.account){
+      console.log('有帳號')
+      router.push('/forum/add')
+    }else{
+      console.log('沒有帳號')
+      Swal.fire({
+        title: '您尚未登入',
+        text: "要登入以編輯網站嗎?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: '帶我去登入'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          router.push('/login')
+        }else{
+
+        }
+      })
+    }
+  }
+
   return (
     <>
       <div className={styles.bigcontainer}>
@@ -11,13 +50,13 @@ export default function Hotnew() {
           <div className={styles.ptext}>最新</div>
         </div>
         <div className={styles.container2}>
-          <Link href="/forum/add">
+          {/* <Link href="/forum/add" onClick={checkLogin}> */}
             {' '}
             {/* 使用 Link 組件包裹要連結的元素 */}
             <div className={styles.ptext}>
-              <BsPencilSquare />
+              <BsPencilSquare onClick={checkLogin}/>
             </div>
-          </Link>
+          {/* </Link> */}
           <div className={styles.ptext}>我的文章</div>
         </div>
       </div>
