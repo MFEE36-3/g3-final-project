64a865bb5079954c409a05aa16a6a749d0df2cac
sign <sign1113@ymail.com>
2023-08-07 14:36:39 +0800

    8/7

--- components/common/forum/messageinput.js
+++ components/common/forum/messageinput.js
@@ -4,64 +4,66 @@ import { RiSendPlane2Fill } from 'react-icons/ri';
 import { useRouter } from 'next/router';
 import AuthContext from '@/context/AuthContext';
 export default function MessageInput({ messages }) {
-  console.log(messages)
+  console.log(messages);
   const [message, setMessage] = useState('');
-  
+
   const { auth } = useContext(AuthContext);
   const router = useRouter();
   const [comment_content, setComment_Content] = useState('');
 
   // 從後端資料庫拿到使用者的大頭貼
-  const [userPhoto,setUserPhoto] = useState({
-    user_id:'',
-    user_photo:''
-  })
+  const [userPhoto, setUserPhoto] = useState({
+    user_id: '',
+    user_photo: '',
+  });
 
   // 發送留言到後端資料庫的物件:
-  const [sendMessage,setSendMessage] = useState({
-    member_id:'',
-    content:'',
-    forum_sid:'',
-  })
+  const [sendMessage, setSendMessage] = useState({
+    member_id: '',
+    content: '',
+    forum_sid: '',
+  });
 
-  useEffect(()=>{
-    if(messages.length && messages[0].forum_sid){
-      setSendMessage({...sendMessage,
-        forum_sid:messages[0].forum_sid
-      })
+  useEffect(() => {
+    if (messages.length && messages[0].forum_sid) {
+      setSendMessage({ ...sendMessage, forum_sid: messages[0].forum_sid });
     }
-  },[messages])
+  }, [messages]);
 
-  useEffect(()=>{
-    if(auth.account){
-      setSendMessage({...sendMessage,
-        member_id:auth.sid,
+  useEffect(() => {
+    if (auth.account) {
+      setSendMessage({
+        ...sendMessage,
+        member_id: auth.sid,
         // forum_sid:messages.forum_sid
-      })
+      });
 
-      fetch(process.env.API_SERVER + '/forum/getUserPhoto',{
+      fetch(process.env.API_SERVER + '/forum/getUserPhoto', {
         method: 'POST',
         body: JSON.stringify(auth),
         headers: {
           'Content-Type': 'application/json',
-        }
+        },
       })
-      .then(r => r.json())
-      .then(data => {
-        console.log(data)
-        setUserPhoto({...userPhoto,user_id:data.sid,user_photo:data.photo})
-      })
+        .then((r) => r.json())
+        .then((data) => {
+          console.log(data);
+          setUserPhoto({
+            ...userPhoto,
+            user_id: data.sid,
+            user_photo: data.photo,
+          });
+        });
     }
-  },[auth])
+  }, [auth]);
 
   // console.log(sendMessage)
   // console.log(messages)
-  
+
   // 將留言內容加到要傳送到後端的物件狀態
   const handleAddContent = (e) => {
-    setSendMessage({...sendMessage,[e.target.name]:e.target.value})
-  }
-
+    setSendMessage({ ...sendMessage, [e.target.name]: e.target.value });
+  };
 
   // 留言新增函式
   // const handlecontent = (e) => {
@@ -100,50 +102,52 @@ export default function MessageInput({ messages }) {
     }
   };
 
-  const addMessage = (e)=>{
-    e.preventDefault()
+  const addMessage = (e) => {
+    e.preventDefault();
 
-    fetch(process.env.API_SERVER + '/forum/addmessage',{
+    fetch(process.env.API_SERVER + '/forum/addmessage', {
       method: 'POST',
       body: JSON.stringify(sendMessage),
       headers: {
         'Content-Type': 'application/json',
-      }
+      },
     })
-    .then(r => r.json())
-    .then(data => {
-      console.log(data)
-      // 記得把這個打開
-      // router.push(`/forum/${rid}`)
-    })
-  }
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        // 記得把這個打開
+        // router.push(`/forum/${rid}`)
+      });
+  };
 
   return (
     <>
       {/* <form onSubmit={()=>{}} className={styles.form}> */}
-        <div className={styles.inputContainer}>
-          {/* {messages.map((msg, index) => (
+      <div className={styles.inputContainer}>
+        {/* {messages.map((msg, index) => (
             <div key={index} className={styles.avatar}>
               <img src={`http://localhost:3002/img/member/${msg.photo}`} />
             </div>
           ))} */}
 
-          <div className={styles.avatar}>
-              <img src={`http://localhost:3002/img/member/${userPhoto.user_photo}`} />
-            </div>
-
-          <input
-            className={styles.input}
-            type="text"
-            name='content'
-            value={sendMessage.content}
-            onChange={handleAddContent}
-            placeholder="輸入訊息..."
+        <div className={styles.avatar}>
+          <img
+            src={`http://localhost:3002/img/member/${userPhoto.user_photo}`}
           />
-          <button type="submit" className={styles.iconButton}>
-            <RiSendPlane2Fill className={styles.icon} onClick={addMessage}/>
-          </button>
         </div>
+
+        <input
+          className={styles.input}
+          type="text"
+          name="content"
+          value={sendMessage.content}
+          onChange={handleAddContent}
+          placeholder="輸入訊息..."
+        />
+        <button type="submit" className={styles.iconButton}>
+          <RiSendPlane2Fill className={styles.icon} onClick={addMessage} />
+        </button>
+      </div>
       {/* </form> */}
     </>
   );
