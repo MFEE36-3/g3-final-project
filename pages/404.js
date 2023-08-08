b070f127e6aebb2e09bbc9aafff78e9702f6a111
Daidai <sc931612@gmail.com>
2023-08-08 16:13:24 +0800

    404page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+import filled_walkbag_middle from '@/public/main_page/filled_walkbag_middle.svg';
+import styles from '@/styles/not_found_page.module.css'
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (<>
+    <div className={styles.outer_box}>
+      <Image src={filled_walkbag_middle} className={styles.walk_bag} alt='filled_walkbag_middle' />
+      Oops!沒有這個頁面哦！
+    </div>
+  </>)
 }
