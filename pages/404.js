5f5b6b25941182b3045b2660da8fce42bfed608b
Daidai <sc931612@gmail.com>
2023-08-10 01:55:11 +0800

    404 title

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,13 @@
 import Image from 'next/image';
 import filled_walkbag_middle from '@/public/main_page/filled_walkbag_middle.svg';
-import styles from '@/styles/not_found_page.module.css'
+import styles from '@/styles/not_found_page.module.css';
+import Head from 'next/head';
 
 export default function Custom404() {
   return (<>
+    <Head>
+      <title>食GOEAT! / Oops!</title>
+    </Head>
     <div className={styles.outer_box}>
       <Image src={filled_walkbag_middle} className={styles.walk_bag} alt='filled_walkbag_middle' />
       Oops!沒有這個頁面哦！
